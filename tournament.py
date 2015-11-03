1164e91d89422df6924a27ada93cab9bd1a2c2b2
Jonathan Frazier <JohnFrazier@users.noreply.github.com>
2015-11-03 17:16:18 +0000

    feat: all tournament_tests pass
    Includes all functionality to pass tests.

--- tournament.py
+++ tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -8,37 +8,59 @@ import psycopg2
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    db = psycopg2.connect("dbname=tournament")
+    cur = db.cursor()
+    return db, cur
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    db, cur = connect()
+    cur.execute("delete from matches;")
+    db.commit()
+    cur.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db, cur = connect()
+    cur.execute("delete from players;")
+    db.commit()
+    cur.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db, cur = connect()
+    cur.execute("select count(id) from players;")
+    # remove tuple wrapper from fetched result.
+    (count,) = cur.fetchone()
+    cur.close()
+    # return 0 instead of None when there are no players registered
+    ret = count if count else 0
+    return ret
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    db, cur = connect()
+    cur.execute("insert into players ( name ) values ( %s );", (name,))
+    db.commit()
+    cur.close()
 
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -47,6 +69,10 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    db, cur = connect()
+    cur.execute("select * from standings;")
+    ret = cur.fetchall()
+    return ret
 
 
 def reportMatch(winner, loser):
@@ -56,16 +82,41 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    winning_score = 3
+    losing_score = 0
+
+    # insert into matches, return new match id.
+    db, cur = connect()
+    cur.execute(
+        "insert into matches (player1_id, player2_id) "
+        "values (%s, %s) returning id;", (winner, loser))
+    (matchid,) = cur.fetchone()
+
+    # create player stats entry for each player
+
+    cur.execute(
+        "insert into player_game_stats "
+        "(match_id, player_id, score, player_won) values "
+        "(%s, %s, %s, %s);",
+        (matchid, winner, winning_score, True))
+
+    cur.execute(
+        "insert into player_game_stats "
+        "(match_id, player_id, score, player_won) values "
+        "(%s, %s, %s, %s);",
+        (matchid, loser, losing_score, False))
+
+    db.commit()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -74,4 +125,11 @@ def swissPairings():
         name2: the second player's name
     """
 
-
+    db, cur = connect()
+    # fetch list of players ordered by wins
+    cur.execute("select id,name from standings;")
+    p = cur.fetchall()
+    # slice players list and pair up top players
+    ret = [(a[0], a[1], b[0], b[1]) for a, b in zip(p[::2], p[1::2])]
+    print ret
+    return ret
--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,30 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+create table players (id serial primary key, name varchar(80));
+
+
+create table matches (
+	id serial primary key, player1_id integer, player2_id integer);
+
+
+create table player_game_stats(
+	-- create a row for each player and game
+	id serial primary key, match_id integer, player_id integer,
+	score integer, player_won boolean);
+
+
+-- standings view returns id, name, win count, and match count
+create view standings as
+	select players.id, players.name,
+	-- count number of entries where player_won is true
+	count(nullif(player_won = false, true)) as wins,
+	-- count game stats to get matches
+	count(player_game_stats) as matches
+	-- join players and game stats by id
+	-- right join to include players that haven't played yet
+	from player_game_stats right join players
+	on player_game_stats.player_id = players.id
+	group by players.id
+	order by wins desc;
 
